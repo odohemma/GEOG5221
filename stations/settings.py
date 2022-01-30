9f0c87ad5f5aa50a6d6323381049bfe4863d4d6b
Emmanuel Odoh <odohemma@gmail.com>
2022-01-30 09:26:32 +0100

    added secret to heroku env variables

--- stations/settings.py
+++ stations/settings.py
@@ -40,7 +40,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-s4)-@-n&(8ari8@%)*%izs9e(6okacp!gzp&-*uxsx1avc_@bd'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
